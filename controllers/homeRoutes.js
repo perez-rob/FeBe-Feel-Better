ba5ab37447c2744c4946782e696af0f66ae47c8c
David <dlm.kim617@gmail.com>
2021-07-29 12:32:28 -0400

    pushing-all-api-routes-and-pushing-auth.js-file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,12 @@
+const router = require("express").Router();
+const { Mood, User, Activity } = require("../models");
+const Op = require("sequelize").Op;
+const withAuth = require('../utils/auth');
+// import auth middleware
 
+
+router.get("/", async (req, res) => {
+  res.render("loginPage", {});
+});
+
+module.exports = router;
