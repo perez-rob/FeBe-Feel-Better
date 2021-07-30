d6f1666b85f262d569f5e28d5e55337f01fdf603
bob.w.perez <bob.w.perez@gmail.com>
2021-07-30 03:30:42 -0400

    added password-confirm input for signup form and toast alert if they dont match. changed login logic to allow user to login with either email OR username.

--- public/js/login.js
+++ public/js/login.js
@@ -1,43 +1,49 @@
-const loginForm =async (event) => {
-    event.preventDefault();
-    const email=document.getElementById('email-login').value.trim();
-    const password=document.getElementById('email-login').value.trim();
+const loginForm = async (event) => {
+  event.preventDefault();
+  const userInfo = document.getElementById("user-login").value.trim();
+  const password = document.getElementById("password-login").value.trim();
 
-    if(email&&password){
-        const res = await fetch ('/api/user/login', {
-            method: 'POST',
-            body: JSON.stringify({email,password}),
-            headers: {'Content-Type': 'application/json'},
-        });
-        if(res.ok){
-            document.location.replace('/profile');
-        }else {
-            alert(res.statusText);
-        }
+  if (userInfo && password) {
+    const res = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ userInfo, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (res.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(res.statusText);
     }
+  }
 };
 
-const signUp= async (event) => {
-    event.preventDefault();
-    const name=document.getElementById('name-signup').value.trim();
-    const email=document.getElementById('email-signup').value.trim();
-    const password=document.getElementById('password-signup').value.trim();
+const signUp = async (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username-signup").value.trim();
+  const email = document.getElementById("email-signup").value.trim();
+  const password = document.getElementById("password-signup").value.trim();
+  const passwordConfirm = document
+    .getElementById("password-confirm")
+    .value.trim();
 
-    if(name&&email&&password){
-        const res = await fetch ('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({name,email,password}),
-            headers: {'Content-Type': 'application/json'},
-        });
-        if(res.ok){
-            document.location.replace('/profile');
-        }else {
-            alert(res.statusText);
-        }
+  if (password !== passwordConfirm) {
+    M.toast({ html: "Passwords must match!" });
+    return;
+  }
+
+  if (username && email && password) {
+    const res = await fetch("/api/user", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (res.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(res.statusText);
     }
+  }
 };
 
-document.getElementsByClassName('login-form');
-document.addEventListener('submit',loginForm);
-document.getElementsByClassName('signup-form');
-document.addEventListener('submit',signUp);
+document.getElementById("login-form").addEventListener("submit", loginForm);
+document.getElementById("signup-form").addEventListener("submit", signUp);
